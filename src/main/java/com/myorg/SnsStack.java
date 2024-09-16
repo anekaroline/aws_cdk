c8c97a65fbf94ecc434dd7b8c31e19a2d9333d89
Ane Karoline da Silva Pereira <anekarolinespereira@gmail.com>
2024-09-15 23:36:48 -0400

    Projeto aws que faz parte do curso pratico com java

---
+++ src/main/java/com/myorg/SnsStack.java
@@ -0,0 +1,37 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
+import software.amazon.awscdk.services.sns.Topic;
+import software.amazon.awscdk.services.sns.subscriptions.EmailSubscription;
+import software.constructs.Construct;
+
+
+public class SnsStack extends Stack {
+    private final SnsTopic snsTopic;
+    public SnsStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public SnsStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        snsTopic = SnsTopic.Builder.create(Topic.Builder.create(this, "ProductEventsTopic")
+                .topicName("product-events")
+                .build())
+                .build();
+
+        snsTopic.getTopic().addSubscription(EmailSubscription.Builder.create("anekaroline659@gmail.com")
+                .json(true)
+                .build());
+
+
+
+
+    }
+
+    public SnsTopic getSnsTopic() {
+        return snsTopic;
+    }
+}
